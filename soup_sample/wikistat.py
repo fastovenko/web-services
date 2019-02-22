6c02449d08b1708f8aeba8db6aa267464c460297
greykoshak <greykoshak@gmail.com>
2019-02-22 02:18:56 +0200

    Beautiful soup Short path v5 (почти ok)

--- soup_sample/wikistat.py
+++ soup_sample/wikistat.py
@@ -14,6 +14,7 @@ def cleanup_list(raw_list, undefined_list):
 # Получить список неотмеченных файлов
 def get_undefined_list(dict_wiki_files):
     undefined_list = [wiki for wiki in dict_wiki_files if dict_wiki_files[wiki] == None]
+
     return undefined_list
 
 
@@ -24,7 +25,6 @@ def get_undefined_list(dict_wiki_files):
 #   files: словарь, который изменяем алгоритмом
 #   page_stop: страница, при достижении которой алгоритм останавливается
 def recursion_tree(pages_list, undefined_list, attr_list):
-    print(f"Rec---{len(pages_list)}----{len(undefined_list)}---------------")
     if len(pages_list) == 0 or len(undefined_list) == 0:
         return
 
@@ -32,7 +32,7 @@ def recursion_tree(pages_list, undefined_list, attr_list):
     dict_wiki_files = attr_list[1]
     stop = attr_list[2]
 
-    refs = list()
+    set_pages_level = set()
 
     for page in pages_list:
         storage_path = os.path.join(path, page)
@@ -46,62 +46,86 @@ def recursion_tree(pages_list, undefined_list, attr_list):
         article_list = set(re.findall(r"(?<=/wiki/)[\w()]+", body))
         undefined_list = get_undefined_list(dict_wiki_files)
 
-        refs = cleanup_list(article_list, undefined_list)
+        refs_page = cleanup_list(article_list, undefined_list)
+        set_pages_level.update(refs_page)
 
-        for ref in refs:
+        for ref in refs_page:
             dict_wiki_files[ref] = page
             if ref == stop:
                 return
 
+    refs_pages_level = list(set_pages_level)
     undefined_list = get_undefined_list(dict_wiki_files)
-    recursion_tree(refs, undefined_list, attr_list)
+
+    recursion_tree(refs_pages_level, undefined_list, attr_list)
 
 
 def build_tree(start, end, path):
-    link_re = re.compile(r"(?<=/wiki/)[\w()]+")  # Искать ссылки можно как угодно, не обязательно через re
-
     wiki_files = os.listdir(path)
     files = dict.fromkeys(wiki_files)  # Словарь вида {"filename1": None, "filename2": None, ...}
 
     # Первый вызов: стартовая страница, весь список страниц - undefined
     recursion_tree([start], wiki_files, [path, files, end])
 
-    # TODO Проставить всем ключам в files правильного родителя в значение, начиная от start
-    print(files['Brain'])
-    print(files['Artificial_intelligence'])
-    print(files['Python_(programming_language)'])
     return files
 
 
 def build_bridge(start, end, path):
     files = build_tree(start, end, path)
     bridge = []
-    # TODO Добавить нужные страницы в bridge
+    page = end
+
+    while page != start:
+        bridge.append(page)
+        page = files[page]
+
+    bridge.append(start)
+
     return bridge
 
 
-def parse(start, end, path):
-    """
-    Если не получается найти список страниц bridge, через ссылки на которых можно добраться от start
-    до end, то, по крайней мере, известны сами start и end, и можно распарсить хотя бы их:
-    bridge = [end, start]. Оценка за тест, в этом случае, будет сильно снижена, но на минимальный
-    проходной балл наберется, и тест будет пройден.
-    Чтобы получить максимальный балл, придется искать все страницы. Удачи!
-    """
+def count_imgs(source):
+    img_list = source.find_all("img")
+    imgs = 0
 
-    bridge = build_bridge(start, end, path)  # Искать список страниц можно как угодно, даже так:
-    # bridge = [end, start]
-    # Когда есть список страниц, из них нужно вытащить данные и вернуть их
+    for img_current in img_list:
+        if img_current.get("width") != None:
+            width = int(img_current['width'])
+            imgs += 1 if width >= 200 else 0
+    return imgs
+
+
+def count_headers(source):
+    headers_list = source.find_all(name=re.compile('h[1-6]'))
+    headers = 0
+    count = 0
+
+    for header_current in headers_list:
+        if header_current.string != None:
+            if re.search('^[ETC]', header_current.string):
+                count += 1
+                print("ok")
+            headers += 1 if re.search('^[ETC]', header_current.string) else 0
+
+    return headers
+
+
+def parse(start, end, path):
+    bridge = build_bridge(start, end, path)
+
     out = {}
     for file in bridge:
+        print(f"-----{file}---------")
         with open("{}{}".format(path, file)) as data:
             soup = BeautifulSoup(data, "lxml")
 
         body = soup.find(id="bodyContent")
+        imgs = count_imgs(body)
+        headers = count_headers(body)
 
-        # TODO посчитать реальные значения
-        imgs = 5  # Количество картинок (img) с шириной (width) не меньше 200
-        headers = 10  # Количество заголовков, первая буква текста внутри которого: E, T или C
+
+        # imgs = 5  # Количество картинок (img) с шириной (width) не меньше 200
+        # headers = 10  # Количество заголовков, первая буква текста внутри которого: E, T или C
         linkslen = 15  # Длина максимальной последовательности ссылок, между которыми нет других тегов
         lists = 20  # Количество списков, не вложенных в другие списки
 
