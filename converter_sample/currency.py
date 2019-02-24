0bf1bcc11db5972ef3524d834da1f06ae11c4a16
greykoshak <greykoshak@gmail.com>
2019-02-25 00:26:14 +0200

    Beautiful converter start v1

--- converter_sample/currency.py
+++ converter_sample/currency.py
@@ -1,9 +1,41 @@
+import logging
 from bs4 import BeautifulSoup
 from decimal import Decimal
 
+logging.basicConfig(
+    format='[%(asctime)s][LINE:%(lineno)d][%(name)s][%(levelname)s:] %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S',
+    # filename='log/example.log',
+    level=logging.DEBUG)
+logger = logging.getLogger("currency")
+
+API_URL = 'http://www.cbr.ru/scripts/XML_daily.asp?date_req='
+PROXY_DICT = {
+    "http": "http://195.208.172.70:8080",
+    "https": "https://195.208.172.70:8080",
+}
+
 
 def convert(amount, cur_from, cur_to, date, requests):
-    response = requests.get()  # Использовать переданный requests
+    logger.debug(f"Working {API_URL}...")
+
+
+    response = requests.get(API_URL + date, proxies=PROXY_DICT).content
+    soup = BeautifulSoup(response, "xml")
+
+    logger.debug(soup.find('CharCode', text=cur_to).find_next_sibling('Value'))
+
     # ...
     result = Decimal('3754.8057')
     return result  # не забыть про округление до 4х знаков после запятой
+
+
+# import requests
+# http_proxy = "http://195.208.172.70:8080"
+# https_proxy = "https://195.208.172.70:8080"
+# response = requests.get('http://www.cbr.ru/scripts/XML_daily.asp', proxies={"http": http_proxy,"https": https_proxy,}).content
+#
+# from bs4 import BeautifulSoup
+# soup = BeautifulSoup(response, 'xml')
+#
+# soup.find('CharCode', text='EUR').find_next_sibling('Value')
