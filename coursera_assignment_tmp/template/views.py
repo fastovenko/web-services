91e4d5950596cf4e2c0b521c0f3e6c82d32a236a
greykoshak <greykoshak@gmail.com>
2019-03-08 00:31:06 +0200

    Django: second 0:30

--- coursera_assignment_tmp/template/views.py
+++ coursera_assignment_tmp/template/views.py
@@ -2,11 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-# Create your views here.
-# get с: 2
-# get b: 3
-# statement is empty
-
 """
 @register.filter(name='inc')
 def inc(value, arg):
@@ -22,32 +17,6 @@ def division(a, b, to_int=False):
 @register.simple_tag тест проходит. Почему, какие между ними отличия ?    
 
 Вопрос решен. Не хватало проверки на ноль.
-
-Подскажите, пожалуйста, какие именно заголовки запроса нужны? 
-вернуть нужно параметры и значение HTTP_X-PRINT-STATEMENT, если оно передано.
-
-Попробовал простейшие варианты:
-первый
-{% extends "base.html" %}
-{% block title %}new_block{% endblock %}
-{% block content %}
-{{block_a}}
-{{block_b}}
-{% endblock %}
-
-или второй, если убрать все теги нового блока
-{% extends "base.html" %}
-{{block_a}}
-{{block_b}}
-У меня отображается родительская страница целиком. Вопрос: как отобразить один блок из родительской страницы ?
-
-Нашел. Вызвать содержимое родительского блока: {{block.super}}
-
-Обнулить содержимое родительского блока: 
-{% block block_a %}{{a}}{% endblock %} просто не указать ничего в теле. 
-А если в тело вставить super то и как раз получится часть требуемого задания.
-
-
 
 
 def echo(request):
@@ -73,7 +42,12 @@ return render(request, 'echo.html', context={
 """
 
 def echo(request):
-    return HttpResponse(status=200)
+    return render(request, 'echo.html', context={
+        'get_letter': request.META['QUERY_STRING'][0],
+        'get_value': request.GET.get(request.META['QUERY_STRING'][0]),
+        'get_tag': request.META.get('HTTP_X_PRINT_STATEMENT'),
+        'request_method': request.META['REQUEST_METHOD']
+        }, status=200)
 
 
 def filters(request):
